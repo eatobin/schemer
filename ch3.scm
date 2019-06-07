cf2a35e3f5003e910a5aa67a5e5ac4bdf1ea4c9f
Eric Tobin <eatobin@gmail.com>
2019-06-07 16:29:17 -0500

    just scm

--- ch3.scm
+++ ch3.scm
@@ -1,1 +1,10 @@
 (include "ch2.scm")
+
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat?) (quote ()))
+     (else (cond
+            ((eq? (car lat) a) (cdr lat))
+            (else (rember a
+                          (cdr lat))))))))
