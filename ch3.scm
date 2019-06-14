d5a988e0b216e545bfda9944ccd1cf1b5ea44d10
Eric Tobin <eatobin@gmail.com>
2019-06-14 15:25:08 -0500

    insertR!!

--- ch3.scm
+++ ch3.scm
@@ -16,3 +16,11 @@
      ((null? l) (quote ()))
      (else (cons (car (car l))
                  (firsts (cdr l)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+            ((eq? (car lat) old) (cons (car lat) (cons new (cdr lat))))
+            (else (cons (car lat) (insertR new old (cdr lat)))))))))
