b5695fda1a9fc8be15db18c5e132468ba8e27386
Eric Tobin <eatobin@gmail.com>
2019-06-16 18:16:28 -0500

    subst!

--- ch3.scm
+++ ch3.scm
@@ -32,3 +32,11 @@
      (else (cond
             ((eq? (car lat) old) (cons new lat))
             (else (cons (car lat) (insertL new old (cdr lat)))))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+            ((eq? (car lat) old) (cons new (cdr lat)))
+            (else (cons (car lat) (subst new old (cdr lat)))))))))
