18614873b7d6ee31e86ef16759a8f5eb92a1a047
Eric Tobin <eatobin@gmail.com>
2019-06-21 16:03:10 -0500

    progress in cl

--- ch3.lisp
+++ ch3.lisp
@@ -1,24 +1,8 @@
 (load (compile-file "ch2.lisp"))
 
-(defun lat2? (l)
+(defun rember (a lat)
   (cond
-    ((null l) t)
-    ((atom? (car l)) (lat? (cdr l)))
-    (t nil)))
+    ((null lat) (quote ()))
+    ((eq (car lat) a) (cdr lat))
+    (t (cons (car lat) (rember a (cdr lat))))))
 
-(defun member2? (a lat)
-  (cond
-    ((null lat) nil)
-    (t (or (eq (car lat) a)
-           (member? a (cdr lat))))))
-
-(lat? '())
-(lat? '(8))
-(lat? '(8 (8)))
-
-(member? :a '())
-(member? :a '(:a))
-(member? :a '(:ax))
-(member? :a '(:ax :a))
-(member? 'dog '(cat dog))
-(member? 'dog '(cat dogg))
