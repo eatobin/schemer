04b875df22d3f0b01e09625d222d57f26a1532de
Eric Tobin <eatobin@gmail.com>
2019-06-25 14:02:40 -0500

    better cons

--- ch3.scm
+++ ch3.scm
@@ -62,5 +62,5 @@
   (lambda (new old lat)
     (cond
      ((null? lat) (quote ()))
-     ((eq? (car lat) old) (cons new (cons (car lat) (multiinsertL new old (cdr lat)))))
+     ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat)))))
      (else (cons (car lat) (multiinsertL new old (cdr lat)))))))
