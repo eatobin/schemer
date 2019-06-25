88237a076b92e861711302df5f93237f55ab2ebb
Eric Tobin <eatobin@gmail.com>
2019-06-25 13:56:52 -0500

    multiinsertL!!!

--- ch3.scm
+++ ch3.scm
@@ -57,3 +57,10 @@
      ((null? lat) (quote ()))
      ((eq? (car lat) old) (cons (car lat) (cons new (multiinsertR new old (cdr lat)))))
      (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     ((eq? (car lat) old) (cons new (cons (car lat) (multiinsertL new old (cdr lat)))))
+     (else (cons (car lat) (multiinsertL new old (cdr lat)))))))
