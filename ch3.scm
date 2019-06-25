f30fc6e08dd73a51d6aa8ca593cc984f309a60a6
Eric Tobin <eatobin@gmail.com>
2019-06-25 13:19:03 -0500

    all working in 3.scm

--- ch3.scm
+++ ch3.scm
@@ -27,25 +27,22 @@
   (lambda (new old lat)
     (cond
      ((null? lat) (quote ()))
-     (else (cond
-            ((eq? (car lat) old) (cons new lat))
-            (else (cons (car lat) (insertL new old (cdr lat)))))))))
+     ((eq? (car lat) old) (cons new lat))
+     (else (cons (car lat) (insertL new old (cdr lat)))))))
 
 (define subst
   (lambda (new old lat)
     (cond
      ((null? lat) (quote ()))
-     (else (cond
-            ((eq? (car lat) old) (cons new (cdr lat)))
-            (else (cons (car lat) (subst new old (cdr lat)))))))))
+     ((eq? (car lat) old) (cons new (cdr lat)))
+     (else (cons (car lat) (subst new old (cdr lat)))))))
 
 (define subst2
   (lambda (new o1 o2 lat)
     (cond
      ((null? lat) (quote ()))
-     (else (cond
-            ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
-            (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
+     ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+     (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
 
 (define multiinsertR
   (lambda (new old lat)
