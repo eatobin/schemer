da1ac2f9f93f564f3bbe49db3c1aa536039848a3
Eric Tobin <eatobin@gmail.com>
2019-06-27 21:49:53 -0500

    format

--- ch3.scm
+++ ch3.scm
@@ -5,16 +5,13 @@
     (cond
      ((null? lat) (quote ()))
      ((eq? (car lat) a) (cdr lat))
-     (else (cons (car lat)
-                 (rember a
-                         (cdr lat)))))))
+     (else (cons (car lat) (rember a (cdr lat)))))))
 
 (define firsts
   (lambda (l)
     (cond
      ((null? l) (quote ()))
-     (else (cons (car (car l))
-                 (firsts (cdr l)))))))
+     (else (cons (car (car l)) (firsts (cdr l)))))))
 
 (define insertR
   (lambda (new old lat)
