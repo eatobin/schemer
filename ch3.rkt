061a9e231f2cb91866c3d050a16aa087bd4d97c9
Eric Tobin <eatobin@gmail.com>
2019-07-18 12:19:47 -0500

    quack format

--- ch3.rkt
+++ ch3.rkt
@@ -11,53 +11,53 @@
 (define rember
   (lambda (a lat)
     (cond
-      ((null? lat) (quote ()))
-      (else (cond
-              ((eq? (car lat) a) (cdr lat))
-              (else (cons (car lat)
-                          (rember a (cdr lat)))))))))
+     ((null? lat) (quote ()))
+     (else (cond
+            ((eq? (car lat) a) (cdr lat))
+            (else (cons (car lat)
+                        (rember a (cdr lat)))))))))
 
 (define firsts
   (lambda (l)
     (cond
-      ((null? l) (quote ()))
-      (else (cons (car (car l)) (firsts (cdr l)))))))
+     ((null? l) (quote ()))
+     (else (cons (car (car l)) (firsts (cdr l)))))))
 
 (define insertR
   (lambda (new old lat)
     (cond
-      ((null? lat) (quote ()))
-      (else (cond
-              ((eq? (car lat) old) (cons (car lat) (cons new (cdr lat))))
-              (else (cons (car lat) (insertR new old (cdr lat)))))))))
+     ((null? lat) (quote ()))
+     (else (cond
+            ((eq? (car lat) old) (cons (car lat) (cons new (cdr lat))))
+            (else (cons (car lat) (insertR new old (cdr lat)))))))))
 
 (define insertL
   (lambda (new old lat)
     (cond
-      ((null? lat) (quote ()))
-      (else (cond
-              ((eq? (car lat) old) (cons new lat))
-              (else (cons (car lat) (insertL new old (cdr lat)))))))))
+     ((null? lat) (quote ()))
+     (else (cond
+            ((eq? (car lat) old) (cons new lat))
+            (else (cons (car lat) (insertL new old (cdr lat)))))))))
 
 (define subst
   (lambda (new old lat)
     (cond
-      ((null? lat) (quote ()))
-      (else (cond
-              ((eq? (car lat) old) (cons new (cdr lat)))
-              (else (cons (car lat) (subst new old (cdr lat)))))))))
+     ((null? lat) (quote ()))
+     (else (cond
+            ((eq? (car lat) old) (cons new (cdr lat)))
+            (else (cons (car lat) (subst new old (cdr lat)))))))))
 
 (define subst2
   (lambda (new o1 o2 lat)
     (cond
-      ((null? lat) (quote ()))
-      (else (cond
-              ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
-              (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
+     ((null? lat) (quote ()))
+     (else (cond
+            ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+            (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
 
 (define multiinsertR
   (lambda (new old lat)
     (cond
-      ((null? lat) (quote ()))
-      ((eq? (car lat) old) (cons (car lat) (cons new (multiinsertR new old (cdr lat)))))
-      (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
+     ((null? lat) (quote ()))
+     ((eq? (car lat) old) (cons (car lat) (cons new (multiinsertR new old (cdr lat)))))
+     (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
