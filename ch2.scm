92e590e141e58505e788a08752fd53a287395c22
Eric Tobin <eatobin@gmail.com>
2019-07-18 15:26:48 -0500

    broken

--- ch2.scm
+++ ch2.scm
@@ -3,16 +3,16 @@
 (define lat?
   (lambda (l)
     (cond
-      ((null? l) #t)
-      ((atom? (car l))(lat? (cdr l)))
-      (else #f))))
+     ((null? l) #t)
+     ((atom? (car l))(lat? (cdr l)))
+     (else #f))))
 
 (define member?
   (lambda (a lat)
     (cond
-      ((null? lat) #f)
-      (else (or (eq? (car lat) a)
-                (member? a (cdr lat)))))))
+     ((null? lat) #f)
+     (else (or (eq? (car lat) a)
+               (member? a (cdr lat)))))))
 
 (lat? '())
 (lat? '(8))
@@ -23,3 +23,15 @@
 (member? 39 '(22 33 44))
 (member? 'dog '(cat dog))
 (member? 'dog '(cat dogg))
+
+(define flat
+  (lambda (l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l)) (cons (car l) (flat (cdr l))))
+     (else
+      (define inner
+        (lambda (il)
+          (cond
+           ((null? il) (flat (cdr l)))
+           ((atom? (car il)) (cons (car il))))))))))
