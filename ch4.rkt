99693dc77b85983ebf9a91570fb946ef9f96b357
Eric Tobin <eatobin@gmail.com>
2019-07-19 16:30:47 -0500

    progress

--- ch4.rkt
+++ ch4.rkt
@@ -28,3 +28,21 @@
     (cond
      ((zero? m) n)
      (else (sub1 (o- n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+     ((null? tup) 0)
+     (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(define ox
+  (lambda (n m)
+    (cond
+     ((zero? m) 0)
+     (else (o+ n (ox n (sub1 m)))))))
+
+(define (flat l)
+  (cond
+   ((null? l) '())
+   ((atom? (car l)) (cons (car l) (flat (cdr l))))
+   (else (append (flat (car l)) (flat (cdr l))))))
