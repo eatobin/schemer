5ff00630abeb18a4f806df3a58d5dd21a46c8eb3
Eric Tobin <eatobin@gmail.com>
2019-07-19 16:52:17 -0500

    very nice

--- ch4.rkt
+++ ch4.rkt
@@ -49,6 +49,13 @@
      (else (cons (o+ (car tup1) (car tup2))
                  (tup+ (cdr tup1) (cdr tup2)))))))
 
+(define o>
+  (lambda (n m)
+    (cond
+     ((zero? m) #t)
+     ((zero? n) #f)
+     (else (o> (sub1 n) (sub1 m))))))
+
 (define (flat l)
   (cond
    ((null? l) '())
