892ed8bd4e7c48c93a42881a4ee2453fdc749da2
Eric Tobin <eatobin@gmail.com>
2025-04-15 18:12:47 -0700

    uncomment

--- ch1.rkt
+++ ch1.rkt
@@ -3,13 +3,17 @@
 (provide atom?)
 
 (define (atom? x)
-  (and (not (pair? x)) (not (null? x))))
+  (and
+   (not (pair? x))
+   (not (null? x))))
 
 (define my-add
   (lambda (x)
     (+ x 99)))
 
-;; (atom? '())
-;; (atom? '(8))
-;; (atom? 8)
-;; (atom? '((8)))
+(atom? '())
+(atom? '(8))
+(atom? 8)
+(atom? '((8)))
+
+(my-add 15)
